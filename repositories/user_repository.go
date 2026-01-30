34bb4033fa0cd7e8fc5c60d885d3648e3be78292
Muhammad Iqbal <mmuhammadiqbal963@gmail.com>
2026-01-30 17:02:09 +0700

    feat: repository findbyid

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -3,9 +3,12 @@ package repositories
 import (
 	"github.com/iqbal2604/vehicle-tracking-api/config"
 	"github.com/iqbal2604/vehicle-tracking-api/models"
+	"gorm.io/gorm"
 )
 
-type UserRepository struct{}
+type UserRepository struct {
+	DB *gorm.DB
+}
 
 func (r *UserRepository) Create(user *models.User) error {
 	return config.DB.Create(user).Error
@@ -14,6 +17,15 @@ func (r *UserRepository) Create(user *models.User) error {
 func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
 	var user models.User
 	err := config.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) FindByID(id uint) (*models.User, error) {
+	var user models.User
+	err := r.DB.First(&user, id).Error
 	if err != nil {
 		return nil, err
 	}
