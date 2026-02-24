2538c1da3723d502fe4df573d89fd50f38adc9c0
Muhammad Iqbal <mmuhammadiqbal963@gmail.com>
2026-02-24 17:46:36 +0700

    fix: db

--- config/database.go
+++ config/database.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/iqbal2604/vehicle-tracking-api/logs"
 	"github.com/iqbal2604/vehicle-tracking-api/models"
@@ -16,16 +18,23 @@ func ConnectDatabase() error {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=UTC",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var db *gorm.DB
+	var err error
 
-	if err != nil {
-		return err
+	maxAttempts := 12
+	wait := 5 * time.Second
+	for i := 0; i < maxAttempts; i++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			DB = db
+			DB.AutoMigrate(&models.User{}, &models.Vehicle{}, &models.GPSLocation{}, &logs.Log{}, &models.TokenBlacklist{}, &models.Geofence{})
+			return nil
+		}
+		log.Printf("Database not ready (attempt %d/%d): %v", i+1, maxAttempts, err)
+		time.Sleep(wait)
 	}
 
-	DB = db
-
-	DB.AutoMigrate(&models.User{}, &models.Vehicle{}, &models.GPSLocation{}, &logs.Log{}, &models.TokenBlacklist{})
-	return nil
+	return err
 
 }
 
